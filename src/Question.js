de929768d0476d75b7352a787392ed0a578583e8
Zarak <zarak@hotmail.ca>
2020-04-22 19:19:28 +0500

    Fix form submit so that it triggers handler

--- src/Question.js
+++ src/Question.js
@@ -2,25 +2,25 @@ import React from "react";
 import { useSignUpForm } from "./CustomHooks";
 
 export const Question = ({ state }) => {
-  const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
+  const display = () => console.log(inputs);
+  const { inputs, handleInputChange, handleSubmit } = useSignUpForm(display);
   const { selectedIndex } = state;
   const dataActivities = ["cycling", "running", "swimming", "walking"];
   const activity = dataActivities[selectedIndex];
 
   return (
     <div className="row">
-      <form className="col m6 push-m3">
+      <form className="col m6 push-m3" onSubmit={handleSubmit}>
         <p className="flow-text grey-text center">
           How much <span>{activity}</span> have you done today?
         </p>
         <input
           className="grey-text"
-          name=""
+          name="distance"
           type="text"
           value={inputs.activity}
           id={activity}
           placeholder="Distance in m"
-          onSubmit={handleSubmit}
           onChange={handleInputChange}
         />
         <p className="center pink-text error text-lighten-1">Error</p>
