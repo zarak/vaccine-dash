52b7cb9dd8bd4b00001ef67bd4e85d8a7c00e7d7
Zarak <zarak@hotmail.ca>
2020-04-24 12:29:22 +0500

    Use event handlers inside component instead of custom hooks

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,16 +3,24 @@ import { useSignUpForm } from "../CustomHooks";
 import firebase from "../firebase";
 
 export const Question = ({ state }) => {
-  const display = () => console.log(inputs);
-  // const [distance, setDistance] = useState("");
-  const { inputs, handleInputChange, handleSubmit } = useSignUpForm(display);
+  const [distance, setDistance] = useState("");
   const { selectedIndex } = state;
   const dataActivities = ["cycling", "running", "swimming", "walking"];
   const activity = dataActivities[selectedIndex];
 
+  const onChange = event => {
+    setDistance(event.target.value);
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+    setDistance("");
+  };
+
   return (
     <div className="row">
-      <form className="col m6 push-m3" onSubmit={handleSubmit}>
+      <form className="col m6 push-m3" onSubmit={onSubmit}>
         <p className="flow-text grey-text center">
           How much <span>{activity}</span> have you done today?
         </p>
@@ -20,10 +28,10 @@ export const Question = ({ state }) => {
           className="grey-text"
           name="distance"
           type="text"
-          value={inputs.activity}
+          value={distance}
           id={activity}
           placeholder="Distance in m"
-          onChange={handleInputChange}
+          onChange={onChange}
         />
         <p className="center pink-text error text-lighten-1">Error</p>
       </form>
